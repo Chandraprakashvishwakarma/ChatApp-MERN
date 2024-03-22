04ec0de8e3ae1c7f5ac3c9013ba8e17d395cf9ec
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-03-22 13:27:16 +0530

    Changed input from text to textarea

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -15,19 +15,26 @@ const MessageInput = () => {
 
    return (
       <form
-         className="px-4 my-3"
+         className="pl-4 my-3"
          onSubmit={handleSubmit}>
          <div className="relative w-full">
-            <input
+            {/* <input
                type="text"
-               className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
+               className="border text-sm rounded-lg block w-[92%] p-2.5  bg-gray-700 border-gray-600 text-white"
                placeholder="Send a message"
                value={message}
+               onChange={(e) => setMessage(e.target.value)}
+            /> */}
+            <textarea
+               className="border text-sm rounded-lg block w-[92%] p-2.5  bg-gray-700 border-gray-600 text-white"
+               placeholder="Send a message"
+               value={message}
+               rows={1}
                onChange={(e) => setMessage(e.target.value)}
             />
             <button
                type="submit"
-               className="absolute inset-y-0 flex items-center end-0 pe-3">
+               className=" absolute p-2 inset-y-0 w-[8%] flex items-center end-0">
                {loading ? (
                   <div className="loading loading-spinner"></div>
                ) : (
